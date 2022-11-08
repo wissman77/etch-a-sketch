dd2aa4267b74acc22b54a784baf5da2ace02c4f4
Wissam Mansour <wissman77@gmail.com>
2022-11-08 17:21:27 +0200

    Add script to produce all divs and add event listener to every div and add basic style to div

---
+++ script.js
@@ -0,0 +1,21 @@
+// main conatiner DOM element
+const conatiner = document.querySelector('.container');
+let squaresNumber = 16
+// calculating the percentage of width or hight
+let elementHightAndWidth = 100/squaresNumber;
+
+// creating the grid and add event listeners to div elements
+for(let i = 0; i < squaresNumber; i++) {
+  for(let j = 0; j < squaresNumber; j++) {
+    const div = document.createElement('div');
+    div.classList.add('element')
+    div.style.width = `${elementHightAndWidth}%`;
+    div.addEventListener('mouseenter', changeBackground);
+    div.style.height = `${elementHightAndWidth}%`;
+    conatiner.appendChild(div);
+  }
+}
+
+function changeBackground(event) {
+  event.target.style.background = 'black';
+}
