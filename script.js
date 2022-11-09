b9f7728e350def51c9fd4bf9f376611891da8fb6
Wissam Mansour <wissman77@gmail.com>
2022-11-09 13:07:58 +0200

    Add event listener to the change grid button and refactor the produceDivs function

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 // main conatiner DOM element
 const conatiner = document.querySelector('.container');
+const changeGridBtn = document.getElementById('change-grid-btn');
 
 produceDivs(16, conatiner);
 
-function produceDivs(divsNumber, parentNode){
+// Events listeners
+changeGridBtn.addEventListener('click', () => {
+  let squaresNumber = parseInt(prompt('What is the number of squares per side?'));
+  if (squaresNumber > 100 || squaresNumber < 2) {
+    alert('Please provide a number between 2 and 100');
+    return;
+  }
+  clearContainer();
+  produceDivs(squaresNumber, conatiner);
+})
+
+function clearContainer() {
+  conatiner.innerHTML = null;
+}
+
+function produceDivs(squaresNumber, parentNode){
   // calculating the percentage of width or hight
-  let elementHightAndWidth = 100/divsNumber;
+  let elementHightAndWidth = 100/squaresNumber;
 
   // creating the grid and add event listeners to div elements
-  for(let i = 0; i < divsNumber ** 2; i++) {
+  for(let i = 0; i < squaresNumber ** 2; i++) {
     const div = document.createElement('div');
       div.classList.add('element')
       div.style.width = `${elementHightAndWidth}%`;
