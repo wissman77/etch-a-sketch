c12c41a5f056447a85fc83e78f35e3e6f7c68156
Wissam Mansour <wissman77@gmail.com>
2022-11-09 12:25:53 +0200

    refactor to fanction to produce divs

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 // main conatiner DOM element
 const conatiner = document.querySelector('.container');
-let squaresNumber = 16
-// calculating the percentage of width or hight
-let elementHightAndWidth = 100/squaresNumber;
 
-// creating the grid and add event listeners to div elements
-for(let i = 0; i < squaresNumber ** 2; i++) {
-  const div = document.createElement('div');
-    div.classList.add('element')
-    div.style.width = `${elementHightAndWidth}%`;
-    div.addEventListener('mouseenter', changeBackground);
-    div.style.height = `${elementHightAndWidth}%`;
-    conatiner.appendChild(div);
+produceDivs(16, conatiner);
+
+function produceDivs(divsNumber, parentNode){
+  // calculating the percentage of width or hight
+  let elementHightAndWidth = 100/divsNumber;
+
+  // creating the grid and add event listeners to div elements
+  for(let i = 0; i < divsNumber ** 2; i++) {
+    const div = document.createElement('div');
+      div.classList.add('element')
+      div.style.width = `${elementHightAndWidth}%`;
+      div.addEventListener('mouseenter', changeBackground);
+      div.style.height = `${elementHightAndWidth}%`;
+      parentNode.appendChild(div);
+  }
 }
 
 function changeBackground(event) {
